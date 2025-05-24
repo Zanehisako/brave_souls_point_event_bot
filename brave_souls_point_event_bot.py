ae1ebb801b5ba6a943a96fff88099fb37d49d4e1
zanehisako <yassinetakiko@gmail.com>
2025-05-24 23:23:01 +0100

    add an ocr to get the number of tickets so when we run out of em we collect the ticket from the gifts

--- brave_souls_point_event_bot.py
+++ brave_souls_point_event_bot.py
@@ -1,7 +1,20 @@
 import pyautogui
 import time
+import pytesseract
+
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract'
 
 pyautogui.PAUSE = 2
+
+def get_tickets():
+        pyautogui.screenshot('tickets_number.png',region=[920,20,40,30])
+        ticketsNumber = int(pytesseract.image_to_string('tickets_number.png',lang='eng',config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789'))
+        print("ticketsNumber:",ticketsNumber)
+        return ticketsNumber
+    # except:
+    #     print("error ticktes screen capture")
 
 def single_run_point_event():
     state = 0
@@ -74,7 +87,50 @@ def single_run_point_event():
                     print("didnt find the events button")
     print("the single run finished successfully")
 
+def multiple_runs():
+    noTicketsLeft= False
+    numberTickets =get_tickets()
+    while(noTicketsLeft != True):
+        if (numberTickets>=10):
+            single_run_point_event()
+            time.sleep(3)
+            numberTickets =get_tickets()
+        else:
+            try:
+                #checks if there are no ticket gifts
+                pyautogui.locateCenterOnScreen("no_tickets.png",minSearchTime=3,confidence=0.5)
+                print("no more tickets")
+
+                #clicks the close button
+                pyautogui.click(x=965,y=975)
+
+                noTicketsLeft=True
+
+            except:
+                #clicks the menu button
+                pyautogui.click(x=1875,y=60)
+
+                #clicks the gift-box button
+                pyautogui.click(x=1825,y=350)
+
+                #clicks the claim all button
+                pyautogui.click(x=400,y=970)
+
+                #clicks the ok confirme button
+                pyautogui.click(x=1235,y=745)
+
+                #clicks the close confirme button
+                pyautogui.click(x=965,y=975)
+
+                #clicks the close button
+                pyautogui.click(x=965,y=975)
+
+    
+    print("out of tickets!\nEnded the runs")
+
+
 print("starting the script after 5 sec")
 time.sleep(5)
 print("script started !")
-single_run_point_event()
+#single_run_point_event()
+multiple_runs()
